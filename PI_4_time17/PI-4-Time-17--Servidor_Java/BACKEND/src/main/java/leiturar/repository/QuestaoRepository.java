3d9a4a5f19ad5e626016fd4f8bcb7eb9c683418c
luccaescova <160538429+luccaescova@users.noreply.github.com>
2025-11-25 10:19:41 -0300

    Revert "Add files via upload"
    This reverts commit e97eeefd7797c9d0c8f7e1709448e8238af61c4a.

--- PI_4_time17/PI-4-Time-17--Servidor_Java/BACKEND/src/main/java/leiturar/repository/QuestaoRepository.java
+++
@@ -1,9 +0,0 @@
-package main.java.leiturar.repository;
-
-import leiturar.model.Questao;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface QuestaoRepository extends MongoRepository<Questao, String> {
-    List<Questao> findByIdLivro(String idLivro);
-}
--- PI_4_time17/PI-4-Time-17--Servidor_Java/BACKEND/src/main/java/leiturar/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package main.java.leiturar.repository;
-
-import leiturar.model.Usuario;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface UsuarioRepository extends MongoRepository<Usuario, String> {
-    Optional<Usuario> findByEmail(String email);
-}
